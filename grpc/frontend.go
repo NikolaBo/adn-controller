fa527a9aeb0473ecc9220ce4deb9a32d81427f0b
nikolabo <nikolabo@live.com>
2024-03-15 00:02:34 -0500

    add cache and mutate interceptors

--- grpc/frontend.go
+++ grpc/frontend.go
@@ -11,51 +11,60 @@ import (
 
 	// "github.com/UWNetworksLab/adn-controller/grpc/interceptors/null"
 	"github.com/UWNetworksLab/adn-controller/grpc/interceptors/acl"
+	"github.com/UWNetworksLab/adn-controller/grpc/interceptors/cache"
+	"github.com/UWNetworksLab/adn-controller/grpc/interceptors/mutate"
 
 	echo "github.com/UWNetworksLab/adn-controller/grpc/pb"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	request_body := strings.Replace(request.URL.String(), "/", "", -1)
-	fmt.Printf("Got request with body: %s\n", request_body)
-
+func initHandler() (func(writer http.ResponseWriter, request *http.Request), func()) {
 	var conn *grpc.ClientConn
 	// conn, err := grpc.Dial("echo-server:9000", grpc.WithInsecure())
 
 	// nullOpts := []null.CallOption{null.WithMessage("Null"),}
-	aclOpts := []acl.CallOption{acl.WithContent("client"),}
-
+	aclOpts := []acl.CallOption{acl.WithContent("client")}
 
 	conn, err := grpc.Dial(
-			"echo-server:9000", 
-			grpc.WithInsecure(),
-			grpc.WithChainUnaryInterceptor(
-				// null.NullClient(nullOpts...),
-				acl.ACLClient(aclOpts...),
-			),
-			)	
+		"server:9000",
+		grpc.WithInsecure(),
+		grpc.WithChainUnaryInterceptor(
+			// null.NullClient(nullOpts...),
+			acl.ACLClient(aclOpts...),
+			mutate.MutateClient(),
+			cache.CacheClient(),
+		),
+	)
 	if err != nil {
 		log.Fatalf("could not connect: %s", err)
 	}
-	defer conn.Close()
 
-	c := echo.NewEchoServiceClient(conn)
+	return func(writer http.ResponseWriter, request *http.Request) {
+			request_body := strings.Replace(request.URL.String(), "/", "", -1)
+			fmt.Printf("Got request with body: %s\n", request_body)
 
-	message := echo.Msg{
-		Body: request_body,
-	}
+			c := echo.NewEchoServiceClient(conn)
 
-	response, err := c.Echo(context.Background(), &message)
-	if err != nil {
-		fmt.Fprintf(writer, "Echo server returns an error.\n")
-		log.Printf("Error when calling echo: %s", err)
-	} else {
-		fmt.Fprintf(writer, "Echo request finished! Length of the request is %d\n", len(response.Body))
-		log.Printf("Response from server: %s", response.Body)
-	}
+			message := echo.Msg{
+				Body: request_body,
+			}
+
+			response, err := c.Echo(context.Background(), &message)
+			if err != nil {
+				fmt.Fprintf(writer, "Echo server returns an error.\n")
+				log.Printf("Error when calling echo: %s", err)
+			} else {
+				fmt.Fprintf(writer, "Echo request finished! Length of the request is %d\n", len(response.Body))
+				log.Printf("Response from server: %s", response.Body)
+			}
+		}, func() {
+			conn.Close()
+		}
 }
 
 func main() {
+	handler, cleanUp := initHandler()
+	defer cleanUp()
+
 	http.HandleFunc("/", handler)
 
 	fmt.Printf("Starting frontend pod at port 8080\n")
